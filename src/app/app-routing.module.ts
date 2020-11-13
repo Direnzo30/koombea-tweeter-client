e818feb72b928d3d76d544245bb2dd198f16b887
Salvador Di Renzo <direnzosalvatore@gmail.com>
2020-11-13 19:50:23 -0400

    advance on login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
-  { path: 'signin', component: SigninComponent },
-  { path: 'signup', component: SigninComponent },
+  { path: '', component: SigninComponent },
+  { path: 'signup', component: SignupComponent },
 ];
 
 @NgModule({

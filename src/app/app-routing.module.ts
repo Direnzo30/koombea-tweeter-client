caa87fd5a2ceb8413fd61ca3ef3648080e7386b2
Salvador Di Renzo <direnzosalvatore@gmail.com>
2020-11-15 15:28:49 -0400

    advance on dashbboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: '', component: SigninComponent },
   { path: 'signin', component: SigninComponent },
+  { path: 'dashboard', component: DashboardComponent },
   { path: 'signup', component: SignupComponent },
 ];
 

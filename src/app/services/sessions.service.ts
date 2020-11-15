40553dbbe13935434a72873fb61d1ca4f5332067
Salvador Di Renzo <direnzosalvatore@gmail.com>
2020-11-14 23:43:23 -0400

    fix routing

--- src/app/services/sessions.service.ts
+++ src/app/services/sessions.service.ts
@@ -17,14 +17,14 @@ export class SessionsService {
   constructor(private http : HttpClient) { }
 
   public signIn(signinForm: any) : Observable<Response> {
-    return this.http.post<Response>(`${this.apiUrl}/signin`, humps.decamelizeKeys(signinForm)).pipe(map(res => humps.camelizeKeys(res) as Response))
+    return this.http.post<Response>(`${this.apiUrl}/sessions/signin`, humps.decamelizeKeys(signinForm)).pipe(map(res => humps.camelizeKeys(res) as Response))
   }
 
   public signUp(signupForm: any) : Observable<Response> {
-    return this.http.post<Response>(`${this.apiUrl}/signup`, humps.decamelizeKeys(signupForm)).pipe(map(res => humps.camelizeKeys(res) as Response))
+    return this.http.post<Response>(`${this.apiUrl}/sessions/signup`, humps.decamelizeKeys(signupForm)).pipe(map(res => humps.camelizeKeys(res) as Response))
   }
 
   public signOut() : Observable<Response> {
-    return this.http.delete<Response>(`${this.apiUrl}/signout`).pipe(map(res => humps.camelizeKeys(res) as Response))
+    return this.http.delete<Response>(`${this.apiUrl}/sessions/signout`).pipe(map(res => humps.camelizeKeys(res) as Response))
   }
 }
